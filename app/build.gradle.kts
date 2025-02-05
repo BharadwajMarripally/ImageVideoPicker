77428df67beeb1a56b28760358fd44c1ada3aab0
Bharadwaj-Android <bharadwaj-android@webappclouds.com>
2025-02-05 16:35:19 +0530

    Updated build.gradle files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
-//    alias(libs.plugins.android.application)
-    alias(libs.plugins.android.library)
+    alias(libs.plugins.android.application)
+//    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     id ("kotlin-kapt")
-    id ("maven-publish")
+//    id ("maven-publish")
 //    id ("com.google.devtools.ksp")
 }
 
@@ -13,11 +13,11 @@ android {
     compileSdk = 35
 
     defaultConfig {
-//        applicationId = "com.bharadwaj.imagevideopicker"
+        applicationId = "com.bharadwaj.imagevideopicker"
         minSdk = 24
         targetSdk = 35
-//        versionCode = 1
-//        versionName = "1.0"
+        versionCode = 1
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
